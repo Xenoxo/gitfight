b224e4432b6dac84d58045eb0ce4453b6dd78e2f
Xenoxo <timwang@Tims-MacBook-Air.local>
2016-03-07 22:53:58 -0800

    Play with basic components and add simple props to render image, name, and link

--- app/index.js
+++ app/index.js
@@ -1,16 +1,51 @@
+var USER_DATA = {
+	name: 'Tim Wang',
+	username: 'Xenoxo',
+	image: 'https://avatars3.githubusercontent.com/u/854726?v=3&s=460'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
-	render:function () {
+var ProfilePic = React.createClass({
+	render: function () {
+		return <img src={this.props.imageUrl} style={{height: 300, width: 300}} />
+	}
+});
+
+
+var Profilelink = React.createClass({
+	render: function () {
 		return (
 			<div>
-			Hello World!
-			</div>
+				<a href={'https://www.github.com/' + this.props.username}>
+				</a>
+			</div>			
 		)
 	}
 });
 
-ReactDOM.render(<HelloWorld />,
+var ProfileName = React.createClass({
+	render:function () {
+		return (
+			<div>{this.props.name}</div>
+		)
+	}
+});
+
+var Avatar = React.createClass({
+	render: function(){
+		return (
+			<div>
+				<ProfilePic imageUrl={this.props.user.image} />
+				<ProfileName name={this.props.user.name} />
+				<Profilelink username={this.props.user.username} />
+			</div>
+		)
+	}
+})
+
+ReactDOM.render(
+	<Avatar user={USER_DATA} />,
 	document.getElementById('app')
 )
