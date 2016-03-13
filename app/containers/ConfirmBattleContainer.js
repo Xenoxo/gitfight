4127ef6cac447fe977c07678d30234a5b0bff999
Xenoxo <timwyc@gmail.com>
2016-03-12 20:18:43 -0800

    Add github API calls and add prop puking to see all returned data

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,6 +1,6 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
-
+var githubHelpers = require('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
   contextTypes: {
@@ -9,20 +9,27 @@ var ConfirmBattleContainer = React.createClass({
 	getInitialState: function() {
 		return {
 			isLoading: true,
-			playerInfo: []
+			playersInfo: []
 		};
 	},
 	componentDidMount: function(){
 		// Fetch info from github then update state
 		var query = this.props.location.query;
-		console.log('QUERY', query);
+		githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+			.then(function (players) {
+				console.log('PLAYERS',players);
+				this.setState({
+					isLoading: false,
+					playersInfo: [players[0], players[1]]
+				});
+			}.bind(this));
 	},
 	render: function() {
 		return (
-			<ConfirmBattle 
+			<ConfirmBattle
 				isLoading={this.state.isLoading}
-				playersInfo={this.state.playerInfo}
-		/>
+				playersInfo={this.state.playersInfo}
+			/>
 		);
 	}
 });
